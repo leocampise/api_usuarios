7a69ec2587eecc70875a8fedced1a75fbb685e25
leocampise <121525234+leocampise@users.noreply.github.com>
2023-07-18 16:40:20 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,99 @@
+from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
+from flask_cors import CORS
+
+from config import config
+
+app=Flask(__name__)
+CORS(app)
+
+conexion=MySQL(app)
+
+@app.route('/usuarios', methods=['GET'])
+# trae todas las filas de la tabla 'obras'
+def listar_obras():
+    try:
+        cursor = conexion.connection.cursor()
+        sql="SELECT id_obras, nombre, zona FROM obras"
+        cursor.execute(sql)
+        datos=cursor.fetchall()
+        usuarios=[]
+        for fila in datos:
+            usuario={'id':fila[0],'nombre':fila[1],'zona':fila[2]}
+            usuarios.append(usuario)
+        return jsonify({'usuarios':usuarios,'mensaje':'usuarios listados'})
+        
+    except Exception as ex:
+        return jsonify({'mensaje':'Error de conexion'})
+
+# trae solo la fila especificada por el id_obras
+@app.route('/usuarios/<id>', methods=['GET'])
+def listar_obra(id):
+    try:
+        cursor = conexion.connection.cursor()
+        sql="SELECT id_obras, nombre, zona FROM obras WHERE id_obras='{0}'".format(id) #format('variable')
+        cursor.execute(sql)
+        datos=cursor.fetchone()
+        if datos != None:
+            usuario={'id':datos[0],'nombre':datos[1],'zona':datos[2]}
+            return jsonify({'usuario':usuario,'mensaje':'usuario encontrado'})
+        else:
+            return jsonify({'mensaje':'usuario no encontrado'})
+        
+    except Exception as ex:
+        return jsonify({'mensaje':'Error de conexion'})
+
+
+# regitrar nuevo usuario
+@app.route('/usuarios', methods=['POST'])
+
+def registrar_usuario():
+    try:
+        #print(request.json)
+        cursor = conexion.connection.cursor()
+        sql="INSERT INTO obras (id_obras,nombre,zona) VALUES('{0}','{1}','{2}')".format(request.json['id_obras'],request.json['nombre'],request.json['zona'])
+        cursor.execute(sql)
+        conexion.connection.commit() # confirma la accion de ingresar nuevo registro
+        return 'Usuario registrado'
+    
+    except Exception as ex:
+        return jsonify({'mensaje':'Error de conexion'})
+
+
+# borrar un registro
+@app.route('/usuarios/<id>', methods=['DELETE'])
+
+def borrar_usuario(id):
+    try:
+       cursor = conexion.connection.cursor()
+       sql="DELETE FROM obras WHERE id_obras = '{0}'".format(id)
+       cursor.execute(sql)
+       conexion.connection.commit()
+       return 'registro borrado'
+   
+    except Exception as ex:
+        return jsonify({'mensaje':'Error de conexion'})
+
+
+# actualizar un registro mediante su id_obras
+@app.route('/usuarios/<id>', methods=['PUT'])
+
+def actualizar_usuario(id):
+    try:
+        cursor = conexion.connection.cursor()
+        sql="UPDATE obras SET nombre='{0}', zona='{1}' WHERE id_obras='{2}'".format(request.json['nombre'],request.json['zona'],id)
+        cursor.execute(sql)
+        conexion.connection.commit() # confirma la accion de ingresar nuevo registro
+        return 'Usuario actualizado'
+    
+    except Exception as ex:
+        return jsonify({'mensaje':'Error de conexion'})
+    
+def pagina_no_encontrada(error):
+    return '<h3>la pagina que buscas no existe...</h3>',404  #codigo de pagina no encontrada
+    
+    
+if __name__ == '__main__':
+    app.config.from_object(config['development'])
+    app.register_error_handler(404,pagina_no_encontrada)
+    app.run()
---
+++ config.py
@@ -0,0 +1,10 @@
+class DevelopmentConfig():
+    DEBUG=True
+    MYSQL_HOST='srv567.hstgr.io'
+    MYSQL_USER='u818004051_julio'
+    MYSQL_PASSWORD='Leo@1960'
+    MYSQL_DB='u818004051_gestion'
+
+config = {
+    'development': DevelopmentConfig
+}
