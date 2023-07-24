0e784e78d5e6add9ce0b0797f892c89a70a97e99
leocampise <leocampise@yahoo.com>
2023-07-24 20:34:47 -0300

    json trad 6

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ trad = {'tradicional':[1,2,3,4,5,6]}
 # trae resultado de la ultima jugada
 def quini6():
     try:
-        return json.load(trad)
+        return json.dumps(trad)
         
     except Exception as ex:
         return jsonify({'mensaje':'Error '})
